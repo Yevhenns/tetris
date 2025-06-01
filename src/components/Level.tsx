c0608f119f4e5798dbb8b80d0d9ea84575881336
Yevhenns <gifestum@gmail.com>
2025-06-01 09:40:49 +0300

    show level

--- src/components/Level.tsx
+++ src/components/Level.tsx
@@ -1,18 +1,48 @@
-import { type FC } from "react";
+import { useState, type FC } from "react";
 
 interface LevelProps {
   speedHandler: (speedCount: number) => void;
 }
 
 export const Level: FC<LevelProps> = ({ speedHandler }) => {
+  const [level, setLevel] = useState(1);
+
   return (
     <>
-      <p>Level</p>
+      <p>Level: {level}</p>
       <div className="flex gap-2">
-        <button onClick={() => speedHandler(500)}>1</button>
-        <button onClick={() => speedHandler(400)}>2</button>
-        <button onClick={() => speedHandler(300)}>3</button>
-        <button onClick={() => speedHandler(200)}>4</button>
+        <button
+          onClick={() => {
+            speedHandler(500);
+            setLevel(1);
+          }}
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            speedHandler(400);
+            setLevel(2);
+          }}
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            speedHandler(300);
+            setLevel(3);
+          }}
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            speedHandler(200);
+            setLevel(4);
+          }}
+        >
+          4
+        </button>
       </div>
     </>
   );
