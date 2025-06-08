25cad2857cc5443d705c1e0ab10d3435bc3ce631
Yevhenns <gifestum@gmail.com>
2025-06-08 10:11:00 +0300

    new figures

--- src/assets/figures.ts
+++ src/assets/figures.ts
@@ -56,6 +56,46 @@ export const figures = [
     maxX: 1,
     maxY: 1,
   },
+  {
+    name: "angle1",
+    coords: [
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+      { x: 1, y: 2 },
+    ],
+    maxX: 2,
+    maxY: 2,
+  },
+  {
+    name: "angle2",
+    coords: [
+      { x: 2, y: 1 },
+      { x: 2, y: 2 },
+      { x: 1, y: 1 },
+    ],
+    maxX: 2,
+    maxY: 2,
+  },
+  {
+    name: "angle3",
+    coords: [
+      { x: 2, y: 2 },
+      { x: 1, y: 2 },
+      { x: 2, y: 1 },
+    ],
+    maxX: 2,
+    maxY: 2,
+  },
+  {
+    name: "angle4",
+    coords: [
+      { x: 1, y: 2 },
+      { x: 1, y: 1 },
+      { x: 2, y: 2 },
+    ],
+    maxX: 2,
+    maxY: 2,
+  },
 ];
 
 export type Figure = (typeof figures)[number];
